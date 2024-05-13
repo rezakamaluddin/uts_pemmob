6ab9e3635bef19103a97ca76152e25f88fc44ef4
rezakamaluddin <108996565+rezakamaluddin@users.noreply.github.com>
2024-05-13 19:52:02 +0700

    Update get_started_page.dart

--- lib/presentation/pages/get_started_page.dart
+++ lib/presentation/pages/get_started_page.dart
@@ -2,8 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:nft_market/presentation/pages/sign_up_page.dart';
-import 'package:nft_market/presentation/pages/sign_up_page.dart';
-
+import 'package:nft_market/presentation/widgets/button_widget.dart';
 import 'package:nft_market/presentation/widgets/page_widget.dart';
 
 class GetStartedPage extends StatelessWidget {
